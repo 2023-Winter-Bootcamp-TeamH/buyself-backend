22c0008b75b0270970f296c5adb8aba21b034806
snake7667 <snake011128@naver.com>
2023-01-14 02:27:37 +0900

    fix: detect.py

--- detect.py
+++ detect.py
@@ -3,7 +3,6 @@ import re
 import torch
 import json
 import pandas as pd
-from flask import jsonify
 
 def object_cords(select_model):  # 탐지한 객체들의 좌표와 레이블을 반환해주는 함수
     return json.loads(select_model.pandas().xyxy[0].to_json(orient="records"))
@@ -22,21 +21,21 @@ def create_txt(select_model): # 텍스트 파일로 만드는 함수
 
     return re.findall('\d+', data[0])
 
-def get_id(num_line):
+def get_id(num_line): # 텍스트파일에서 int값으로 class id를 가져오는 함수
     i = 0
     while i < len(num_line):
         num_line[i] += 1
         i += 1
     return num_line
 
-def delet_txt():
+def delet_txt(): # 사용한 txt파일 삭제하는 함수
     file_path = "class.txt"
 
     if os.path.exists(file_path):
         os.remove(file_path)
 
 
-def predict(img_name):
+def predict(img_name): # YOLOv5모델로 이미지 객체 탐지
     model = torch.hub.load('ultralytics/yolov5', 'custom', 'best.pt')
 
     # image = get_image(img_name)
