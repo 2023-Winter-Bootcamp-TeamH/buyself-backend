a108c8ec724085758fc06086e6fb49badd74d880
snake7667 <snake011128@naver.com>
2023-01-14 02:28:18 +0900

    fix: final_predict API

--- controller/predictController.py
+++ controller/predictController.py
@@ -7,17 +7,24 @@ import tasks
 db = SQLAlchemy()
 routes = Blueprint('predict', __name__)
 
-@routes.route('/api/predict', methods=['POST'])
-def test():
-    if request.method == 'POST':
-        file = request.files["file"]
-        img_name = post_image(file)
-        get_url(img_name)
-        use_url(img_name)
-        task = tasks.prediction.delay(img_name)
-        result = []
-        for i in task.get():
-            product = views.get_products_id_list(int(i['id']))
-            result.append({'class_name': product['class_name'], 'price': product['price'], 'img_url': product['img_url']})
 
-        return jsonify(result)
+@routes.route('/api/predict', methods=['POST'])
+def detect_object():
+    if request.method == 'POST':
+        try:
+            image = request.files["file"]
+            img_name = post_image(image)
+            url = get_url(img_name)
+            task = tasks.prediction.delay(url)
+
+            result = []
+            for i in task.get():
+                product = views.get_products_id_list(int(i['id']))
+                result.append(
+                    {'class_name': product['class_name'], 'price': product['price'], 'img_url': product['img_url']})
+
+            delete_image(img_name)
+
+            return jsonify(result)
+        except:
+            abort(500, "Wrong")
