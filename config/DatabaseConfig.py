f172c5a72f3fd8af871a787d22735776ca10666e
GaBaljaintheroom <junsu1222@naver.com>
2023-01-26 01:45:19 +0900

    feat: apply .env

--- config/DatabaseConfig.py
+++ config/DatabaseConfig.py
@@ -1,8 +1,10 @@
-DB_USERNAME = ''
-DB_PASSWORD = ''
-DB_HOST = ''
-DB_SCHEMA = ''
-DB_PORT = ''
+import os
+
+DB_USERNAME = os.environ.get('DB_USERNAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_SCHEMA = os.environ.get('DB_SCHEMA')
+DB_PORT = os.environ.get('DB_PORT')
 
 def getURI():
     return 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(DB_USERNAME, DB_PASSWORD,
--- config/s3bucketConfig.py
+++ config/s3bucketConfig.py
@@ -1,4 +1,6 @@
-ACCESS_KEY_ID = ""
-SECRET_ACCESS_KEY = ""
-S3_BUCKET_REGION = ""
-S3_BUCKET_NAME = ""
+import os
+
+ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
+SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
+S3_BUCKET_REGION = os.environ.get('S3_BUCKET_REGION')
+S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
